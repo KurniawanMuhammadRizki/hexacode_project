0da6437f92e66765d3f76df6ff9dc3b129a627ac
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2022-04-14 07:40:33 +0100

    nyoba ubah file

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-index.php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>test</title>
+</head>
+<body>
+test
+</body>
+</html>
