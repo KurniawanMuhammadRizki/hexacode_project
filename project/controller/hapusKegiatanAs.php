02718015b05b1c1efa8eb7ee101744b017947ea1
emajuniarty <emajuniarty07@gmail.com>
2022-04-21 10:14:38 +0800

    baru

---
+++ project/controller/hapusKegiatanAs.php
@@ -0,0 +1,36 @@
+<?php 
+if(isset($_GET['id_kegiatan'])){ 
+  include('../config/config.php'); 
+
+  $id_kegiatan = $_GET['id_kegiatan']; 
+ 
+  $cek = mysqli_query($koneksi, "SELECT id_kegiatan FROM kegiatan WHERE id_kegiatan='$id_kegiatan'") or die(mysql_error()); 
+ 
+  if(mysqli_num_rows($cek) == 0){ 
+ 
+    echo '<script>window.history.back()</script>'; 
+ 
+ }else{ 
+ 
+  $del = mysqli_query($koneksi, "DELETE FROM kegiatan WHERE id_kegiatan='$id_kegiatan'"); 
+
+  if($del){ 
+ 
+   echo 'Data siswa berhasil di hapus! ';  
+   header('Location: ../asisten/dataKegiatan.php');
+
+  }else{ 
+ 
+   echo 'Gagal menghapus data! '; 
+   header('Location: ../asisten/dataKegiatan.php'); 
+ 
+  } 
+ 
+ } 
+ 
+}else{ 
+ 
+  echo '<script>window.history.back()</script>'; 
+ 
+} ?> 
+  
